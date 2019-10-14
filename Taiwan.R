dbcfc2f4e9161ae5c0bc566217784adec15b099e
B-Tang <btang.tsp@gmail.com>
2019-10-14 19:24:09 -0400

    added compatibility with Chinese text (still work in progress)

--- Taiwan.R
+++ Taiwan.R
@@ -1,5 +1,6 @@
 #-----------you need to install the following packages. this only needs to be done once.
 #install.packages(c('sf', 'foreign', 'tidyverse', 'stringi', 'lwgeom', 'digest'))
+#install.packages(c('showtext'))
 
 #-----------initialize libraries. This needs to be done for each new R session 
 library(sf)
@@ -7,6 +8,7 @@ library(foreign)
 library(tidyverse)
 library(lwgeom)
 library(stringi)
+library(showtext)
 options(stringsAsFactors = FALSE)
 
 #-----------download files
@@ -15,6 +17,11 @@ options(stringsAsFactors = FALSE)
 #-----------set the working directory to wherever you unzipped the downloaded files to
 setwd("/Users/Zangetsu/Documents/Coding Projects/RoadColors/taiwan-latest-free.shp")
 
+font_paths("/Users/Zangetsu/Documents/Coding Projects/RoadColors")
+
+font_add("日本高速公路字体", regular = "日本高速公路字体.otf")
+
+font_families()
 
 #-----------set some basic info about the city you're mapping
 city <- "taipei"
@@ -25,9 +32,9 @@ crs <- 7564 #ESRI projection for mapping. I am using: https://spatialreference.o
 
 
 #-----------set up the road types you want to plot and what colors they should be
-plottypes <-  c('Road', 'Street', 'Avenue', 'Lane', 'Close', 'Way', 'Place', 'Embankment')
-plotcolors <-  c('Road' = '#59c8e5', 'Street' = '#fed032', 'Avenue' ='#4cb580', 'Lane' = '#fe4d64', 'Close' = '#0a7abf',
-                 'Way' = '#2e968c', 'Place' = '#fe9ea5', 'Embankment' = '#fe9ea5', 'Motorway' = "#ff9223", 'Other' = '#cccccc')
+plottypes <-  c('路', '街', '巷', '隧道', '高速公路', '段', '弄', '線')
+plotcolors <-  c('路' = '#59c8e5', '街' = '#fed032', '巷' = '#4cb580', '隧道' = '#fe4d64', '高速公路' = '#0a7abf',
+                 '段' = '#2e968c', '弄' = '#fe9ea5', '線' = '#fe9ea5', 'Motorway' = "#ff9223", 'Other' = '#cccccc')
 
 
 #-----------get to plotting
@@ -50,24 +57,25 @@ allroads$len <- st_length(allroads)
 
 #-----derive road suffixes-----
 #run this line if your suffixes are at the END of the name (e.g. Canal Street)
-allroads$TYPE <- substr(allroads$name, stri_locate_last(allroads$name, regex = " ")[, 1] + 1,  nchar(allroads$name)) %>% stri_trans_general(id = "Title")
+
+allroads$TYPE <- substr(allroads$name, nchar(allroads$name),  nchar(allroads$name)) %>% stri_trans_general(id = "Title")
 
 #run this line if your "suffixes" are at the BEGINNING of the name (e.g. Calle de los Gatos)
 #allroads$TYPE <- substr(allroads$name, 1,  str_locate(allroads$name, " ")[, 1] -1)  %>% stri_trans_general(id = "Title")   #for road prefixes
 
 #--------uncomment and run this code to get the top roads by length.
 #--------i usually run this to decide what road types to plot
-plottype <- allroads %>% select(TYPE,len)
-plottype$geometry <- NULL
-plottype <- subset(plottype, !is.na(TYPE))
-plottype <- plottype %>% group_by(TYPE) %>% summarise(Length = sum(len)) %>% arrange(-Length) 
+#plottype <- allroads %>% select(TYPE,len)
+#plottype$geometry <- NULL
+#plottype <- subset(plottype, !is.na(TYPE))
+#plottype <- plottype %>% group_by(TYPE) %>% summarise(Length = sum(len)) %>% arrange(-Length) 
 
 
 #rename motorways that don't have some other designation
-allroads$TYPE[allroads$fclass == 'motorway' & !(allroads$TYPE %in% plottypes)] <- "Motorway"
+#allroads$TYPE[allroads$fclass == 'motorway' & !(allroads$TYPE %in% plottypes)] <- "Motorway"
 
 #put other roads into their own dataframe
-allroads$TYPE[!(allroads$TYPE %in% plottypes) & allroads$TYPE != 'Motorway'] <- "Other"
+allroads$TYPE[!(allroads$TYPE %in% plottypes) & allroads$TYPE != ''] <- "Other"
 otherroads <- allroads[(allroads$TYPE  == "Other"),]
 allroads <- allroads[(allroads$TYPE  != "Other"),]
 
@@ -78,17 +86,15 @@ blankbg <-theme(axis.line=element_blank(),axis.text.x=element_blank(),
                 panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
                 panel.grid.minor=element_blank(),plot.background=element_blank())
 
-ggplot() + blankbg + theme(panel.grid.major = element_line(colour = "transparent")) + 
+showtext_auto()
+
+ggplot() + blankbg + theme(panel.grid.major = element_line(colour = "transparent"), text = element_text(family = "wqy-microhei")) + 
   geom_sf(data=otherroads, size = .8, aes(color=TYPE)) + 
   geom_sf(data=allroads, size =1, aes(color=TYPE)) + 
-  scale_color_manual(values = plotcolors, guide = "legend") 
+  scale_color_manual(values = plotcolors, guide = "legend")
 
 ggsave(paste0(".", city, ".png"), plot = last_plot(),
        scale = 1, width = 24, height = 36, units = "in",
        dpi = 500)
 
-
-
-
-
-
+showtext_auto(FALSE)
